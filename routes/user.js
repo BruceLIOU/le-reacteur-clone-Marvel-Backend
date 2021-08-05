e17e379bc0464a29f5aeda560045afee643abc82
Bruce LIOU <bruceliou@free.fr>
2021-08-05 16:54:41 +0200

    Fix routes user

--- routes/user.js
+++ routes/user.js
@@ -1,62 +1,75 @@
-// Import packages
+// Import package
 const express = require("express");
-const router = express.Router;
+const router = express.Router();
 const uid2 = require("uid2");
 const SHA256 = require("crypto-js/sha256");
 const encBase64 = require("crypto-js/enc-base64");
 
-// import models
+// Import models
 const User = require("../models/User");
 
-// Route POST /user/signup
+// route POST /user/signup
 router.post("/user/signup", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.fields.email });
 
-    if (!user) {
-      const salt = uid2(64);
-
-      const hash = SHA256(req.fields.password + salt).toString(encBase64);
-
-      const token = uid2(64);
-
-      const newUser = new User({
-        email: req.fields.email,
-        account: {
-          username: req.fields.username,
-        },
-        token: token,
-        hash: hash,
-        salt: salt,
-      });
+    if (user) {
+      res.status(409).json({ message: "This email already has an account" });
     } else {
-      res.status(409).json({ message: "This email already exists." });
+      if (req.fields.email && req.fields.password && req.fields.username) {
+        const token = uid2(64);
+        const salt = uid2(64);
+        const hash = SHA256(req.fields.password + salt).toString(encBase64);
+
+        const newUser = new User({
+          username: req.fields.username,
+          email: req.fields.email,
+          token: token,
+          hash: hash,
+          salt: salt,
+        });
+
+        await newUser.save();
+        res.status(200).json({
+          _id: newUser._id,
+          username: newUser.username,
+          email: newUser.email,
+          token: newUser.token,
+        });
+      } else {
+        res.status(400).json({ message: "Missing parameters" });
+      }
     }
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    //console.log(error.message);
+    res.status(400).json({ error: error.message });
   }
 });
 
-// Route POST /user/login
+// route POST /user/login
 router.post("/user/login", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.fields.email });
-    const password = req.fields.password;
-    const newHash = SHA256(password + user.salt).toString(encBase64);
 
-    if (newHash === user.hash) {
-      res.status(200).json({
-        _id: user.id,
-        token: user.token,
-        accout: user.account,
-      });
+    if (user) {
+      if (
+        SHA256(req.fields.password + user.salt).toString(encBase64) ===
+        user.hash
+      ) {
+        res.status(200).json({
+          _id: user._id,
+          token: user.token,
+          username: user.username,
+        });
+      } else {
+        res.status(401).json({ error: "Unauthorized" });
+      }
     } else {
-      res.status(200).json({ message: "Wrong email or password" });
+      res.status(400).json({ message: "User not found" });
     }
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.json({ error: error.message });
   }
 });
 
-// Routers export
 module.exports = router;
